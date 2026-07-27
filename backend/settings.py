f5c7db2efe91053d00ace2b3e62a2eebac241bfb
Esteban <tu_correo_de_github@ejemplo.com>
2026-07-26 22:48:34 -0500

    settings

--- backend/settings.py
+++ backend/settings.py
@@ -113,8 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/maria/puntos_digitales/puntos_digitales_backend/staticfiles/'
 
 AUTH_USER_MODEL = 'core.UsuarioSistema'
 
